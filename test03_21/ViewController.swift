4a48ac0f00dc2ee0970968a1c18ee0ff57992d1f
VLADIMIR SYLEIMANOV <bannabels@gmail.com>
2021-01-03 21:02:45 +0300

    Initial Commit

---
+++ test03_21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test03_21
+//
+//  Created by Vladimir Syleimanov on 03.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
